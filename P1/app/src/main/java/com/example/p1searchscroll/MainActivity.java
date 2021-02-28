bdf5c75d1b6b67a9cc340743789378189f0c2c9b
Benton Li <cl2597@cornell.edu>
2021-03-01 01:23:14 +0800

    Add 2 Buttons

--- P1/app/src/main/java/com/example/p1searchscroll/MainActivity.java
+++ P1/app/src/main/java/com/example/p1searchscroll/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.p1searchscroll;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.fragment.NavHostFragment;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button s_number = findViewById(R.id.button_num);
+        Button s_byte = findViewById(R.id.button_num);
+        s_number.setText("Search for boring Number");
+        s_number.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SearchNumberActivity.class);
+                startActivity(intent);
+            }
+        });
+        s_byte.setText("Search for ByteDance Products");
+        s_byte.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SearchNumberActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
+
+
+
 }
