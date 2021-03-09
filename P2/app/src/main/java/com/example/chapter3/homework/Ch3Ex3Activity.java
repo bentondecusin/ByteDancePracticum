614b8a7b53d35e83c7029fb09ab40862635e1acc
Benton Li <cl2597@cornell.edu>
2021-03-09 18:05:54 +0800

    Add vanish animation

--- P2/app/src/main/java/com/example/chapter3/homework/Ch3Ex3Activity.java
+++ P2/app/src/main/java/com/example/chapter3/homework/Ch3Ex3Activity.java
@@ -1,8 +1,21 @@
 package com.example.chapter3.homework;
 
 import android.os.Bundle;
+import android.widget.TableLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import com.google.android.material.tabs.TabLayout;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static com.google.android.material.tabs.TabLayout.MODE_FIXED;
 
 /**
  * 使用 ViewPager 和 Fragment 做一个简单版的好友列表界面
@@ -11,18 +24,38 @@ import androidx.appcompat.app.AppCompatActivity;
  * 3. 对于好友列表 Fragment，使用 Lottie 实现 Loading 效果，在 5s 后展示实际的列表，要求这里的动效是淡入淡出
  */
 public class Ch3Ex3Activity extends AppCompatActivity {
-
+    private List<List<String>> contacts = new ArrayList<List<String>>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        String[] fam = new String[]{"Family", "Dad", "Mom", "Dog"};
+        String[] fren = new String[]{"Friends", "李二狗", "小翠", "Dog"};
+        String[] work = new String[]{"Colleague", "老板"};
+        contacts.add(Arrays.asList(fam));
+        contacts.add(Arrays.asList(fren));
+        contacts.add(Arrays.asList(work));
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ch3ex3);
-
-
-
         // TODO: ex3-1. 添加 ViewPager 和 Fragment 做可滑动界面
+        ViewPager vp = findViewById(R.id.view_pager);
+        vp.setAdapter(new FragmentPagerAdapter(getSupportFragmentManager()) {
+            @Override
+            public Fragment getItem(int position) {
+                return new PlaceholderFragment(contacts.get(position));
+            }
 
+            @Override
+            public int getCount() {
+                return 3;
+            }
 
+            @Override
+            public CharSequence getPageTitle(int position){
+                return (CharSequence) contacts.get(position).get(0);
+            }
+        });
 
         // TODO: ex3-2, 添加 TabLayout 支持 Tab
+        TabLayout tl = findViewById(R.id.tab_layout);
+        tl.setupWithViewPager(vp);
     }
 }
--- P2/app/src/main/java/com/example/chapter3/homework/PlaceholderFragment.java
+++ P2/app/src/main/java/com/example/chapter3/homework/PlaceholderFragment.java
@@ -1,34 +1,125 @@
 package com.example.chapter3.homework;
 
 
+import android.animation.ObjectAnimator;
+import android.database.DataSetObserver;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.ScrollView;
+import android.widget.SearchView;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.airbnb.lottie.LottieAnimationView;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class PlaceholderFragment extends Fragment {
+    private List<String> contactList = new ArrayList<>();
+    private LottieAnimationView animationView;
+    private ObjectAnimator vanish;
+    private ListView mListView;
+    public PlaceholderFragment(List<String> contactList){
+        this.contactList = contactList;
+    }
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // TODO ex3-3: 修改 fragment_placeholder，添加 loading 控件和列表视图控件
+        Bundle args = getArguments();
         return inflater.inflate(R.layout.fragment_placeholder, container, false);
     }
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+        animationView = getView().findViewById(R.id.animation_view);
+        mListView = getView().findViewById(R.id.ListView);
+   /*     mListView.setAdapter(new ListViewAdapter(contactList) {
+            @Override
+            public boolean areAllItemsEnabled() {
+                return false;
+            }
 
+            @Override
+            public boolean isEnabled(int position) {
+                return false;
+            }
+
+            @Override
+            public void registerDataSetObserver(DataSetObserver observer) {
+
+            }
+
+            @Override
+            public void unregisterDataSetObserver(DataSetObserver observer) {
+
+            }
+
+            @Override
+            public int getCount() {
+                return 0;
+            }
+
+            @Override
+            public Object getItem(int position) {
+                return null;
+            }
+
+            @Override
+            public long getItemId(int position) {
+                return 0;
+            }
+
+            @Override
+            public boolean hasStableIds() {
+                return false;
+            }
+
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent) {
+                return null;
+            }
+
+            @Override
+            public int getItemViewType(int position) {
+                return 0;
+            }
+
+            @Override
+            public int getViewTypeCount() {
+                return 0;
+            }
+
+            @Override
+            public boolean isEmpty() {
+                return false;
+            }
+        }); */
+        vanish = ObjectAnimator.ofFloat(
+                getView().findViewById(R.id.animation_view),
+                "alpha",
+                1f,0f);
+        vanish.setDuration(3000);
+        vanish.start();
         getView().postDelayed(new Runnable() {
             @Override
-            public void run() {
-                // 这里会在 5s 后执行
+            public void run(){
+
+                animationView.pauseAnimation();
+
+                // 这里会在 3s 后执行
                 // TODO ex3-4：实现动画，将 lottie 控件淡出，列表数据淡入
+
             }
-        }, 5000);
+        }, 3000);
     }
 }
